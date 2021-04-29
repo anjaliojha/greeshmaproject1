23f6d6a555182fe1b2554a12c91c9d776243a8c7
anjaliojha <anjali29.ojha@gmail.com>
2021-04-29 20:42:35 +0530

    corrected

--- sketch.js
+++ sketch.js
@@ -5,32 +5,35 @@ var music;
 
 function preload(){
     // load sound here
+    music=loadSound("music.mp3");
 }
 
 
 function setup(){
     canvas = createCanvas(800,600);
 
-    block1 = createSprite(0,580,360,30);
+    block1 = createSprite(100,580,200,30);
     block1.shapeColor = "blue";
 
-    block2 = createSprite(295,580,200,30);
+    block2 = createSprite(300,580,200,30);
     block2.shapeColor = "orange";
 
-    block3=createSprite(174,445,223,30);
+    block3=createSprite(500,580,200,30);
     block3.shapeColor="green";
 
-    block4=createSprite(497,121,366,30)
-    block4.color="red";
+    block4=createSprite(700,580,200,30)
+    block4.shapeColor="red";
 
     ball = createSprite(random(20,750),100, 40,40);
     ball.shapeColor = rgb(255,255,255); 
-    ball.velocityX=2
-    ball.velocityY=2
+    ball.velocityX=4;
+    ball.velocityY=5;
 }
 
 function draw() {
+
     background(rgb(169,169,169));
+
     edges=createEdgeSprites();
     ball.bounceOff(edges);
 
@@ -49,28 +52,26 @@ function draw() {
         ball.velocityX=0;
         ball.velocityY=0;
         //write code to stop music
-        music.play(false)
+        music.stop();
     }
 
     //write code to bounce off ball from the block3
 
-    if(block2.isTouching(ball)){
-        ball.shapeColor = "orange";
+    if(block3.isTouching(ball)&& ball.bounceOff(block3)){
+        ball.shapeColor = "green";
         //write code to set velocityX and velocityY of ball as 0
-        ball.velocityX=0;
-        ball.velocityY=0;
+       
         //write code to stop music
-        music.play(false)
+        music.play();
     }
     //write code to bounce off ball from the block4
 
-    if(block2.isTouching(ball)){
-        ball.shapeColor = "orange";
+    if(block4.isTouching(ball)&& ball.bounceOff(block4)){
+        ball.shapeColor = "red";
         //write code to set velocityX and velocityY of ball as 0
-        ball.velocityX=0;
-        ball.velocityY=0;
+       
         //write code to stop music
-        music.play(false)
+        music.play();
     }
     drawSprites();
 }
